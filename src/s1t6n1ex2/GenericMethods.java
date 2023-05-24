1a604f56b37ba01b84acf70f20aa184dad8ad160
ArnauBurguera <aburguera.ab@gmail.com>
2023-05-24 12:20:04 +0200

    Exercici acabat

---
+++ src/s1t6n1ex2/GenericMethods.java
@@ -0,0 +1,9 @@
+package s1t6n1ex2;
+
+public class GenericMethods {
+	
+	public static <T> void string(String o1, T o2, T o3) {
+		System.out.println(o1 + "\n" + o2 + "\n" + o3);
+	}
+
+}
---
+++ src/s1t6n1ex2/S1T6n1ex2.java
@@ -0,0 +1,11 @@
+package s1t6n1ex2;
+
+public class S1T6n1ex2 {
+
+	public static void main(String[] args) {
+		
+		GenericMethods.string("Jaume", "Macarró", 23);
+
+	}
+
+}
